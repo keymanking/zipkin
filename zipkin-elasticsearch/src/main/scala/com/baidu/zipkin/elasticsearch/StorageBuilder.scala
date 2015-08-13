0be971d79d56355a859396914690b9214061d48a
liuzhuo <liuzhuo@baidu.com>
2015-08-13 16:06:00 +0800

    range search , use ElasticSearch

--- zipkin-elasticsearch/src/main/scala/com/baidu/zipkin/elasticsearch/StorageBuilder.scala
+++ zipkin-elasticsearch/src/main/scala/com/baidu/zipkin/elasticsearch/StorageBuilder.scala
@@ -1,19 +1,18 @@
 package com.baidu.zipkin.elasticsearch
 
-import com.sksamuel.elastic4s.ElasticClient
-import com.twitter.conversions.time._
-import com.twitter.util.Duration
-import com.twitter.zipkin.builder.Builder
 import com.baidu.zipkin.storage.elasticsearch.ElasticsearchStorage
+import com.sksamuel.elastic4s.ElasticClient
+import com.twitter.zipkin.builder.Builder
 import com.twitter.zipkin.storage.Storage
 
 case class StorageBuilder(
-  client: ElasticClient
+  client: ElasticClient,
+  storeDurationSpanList: Seq[String] = Seq()
 ) extends Builder[Storage] {
 
   def apply() = {
 
-    ElasticsearchStorage(client)
+    ElasticsearchStorage(client, storeDurationSpanList)
   }
 }
 
--- zipkin-elasticsearch/src/main/scala/com/baidu/zipkin/storage/elasticsearch/ElasticsearchStorage.scala
+++ zipkin-elasticsearch/src/main/scala/com/baidu/zipkin/storage/elasticsearch/ElasticsearchStorage.scala
@@ -1,22 +1,16 @@
-
 package com.baidu.zipkin.storage.elasticsearch
 
-import java.nio.CharBuffer
 import java.nio.charset.Charset
-
-import com.twitter.conversions.time._
-import com.twitter.ostrich.stats.Stats
-import com.twitter.util.{Await, Duration, Future}
-import com.twitter.zipkin.common.Span
-import com.twitter.zipkin.conversions.thrift._
-import com.twitter.zipkin.storage.Storage
-import scala.collection.JavaConverters._
 
 import com.sksamuel.elastic4s.ElasticClient
 import com.sksamuel.elastic4s.ElasticDsl._
+import com.twitter.util.{Duration, Future}
+import com.twitter.zipkin.common.Span
+import com.twitter.zipkin.storage.Storage
 
 case class ElasticsearchStorage(
-  client: ElasticClient
+  client: ElasticClient,
+  storeDurationSpanList: Seq[String]
 ) extends Storage {
   val decoder = Charset.forName("UTF-8").newDecoder()
 
@@ -26,34 +20,56 @@ case class ElasticsearchStorage(
   def storeSpan(span: Span): Future[Unit] = {
 
     //val client = ElasticClient.remote("localhost",9300)
-    //ToDo : proto include product name
-    val productName = "www"
-    val serviceName = span.clientSideEndpoint.get.serviceName
+    // 1. in the list
+    // 2. client side span
+    // 3. duration > 0
+    if ( span.isClientSide() && storeDurationSpanList.contains(span.name.split("_").head) ) {
 
-    val metaFields = Map(
-      "trace_id" -> span.traceId,
-      "host" -> span.clientSideEndpoint.get.getHostAddress,
-      "port" -> span.clientSideEndpoint.get.port,
-      "service_name" -> serviceName
-    )
-    // filter out xxx.log in binaryAnnotation
-    // decode binaryAnnotation value
-    val binaryAnnotationFields = span.binaryAnnotations.filter(!_.key.endsWith("log")).
-      map( b => b.key -> decoder.decode(b.value))
+      val first = span.firstAnnotation.map(_.timestamp)
+      val last = span.lastAnnotation.map(_.timestamp)
+      var duration = last.get - first.get
 
-    Future {
-      client.execute {
-        // index metaFields & binaryAnnotationFields
-        index into productName / serviceName fields metaFields ++ binaryAnnotationFields
-      }.await
-      // return Unit
-      ()
-    } onSuccess { (_) =>
-      println("send to es success")
-      println(span.traceId)
-    } onFailure { (_) =>
-      println("error")
+      if ( duration > 0 ) {
+        val duration_index = "duration_index"
+        //ToDo : data should include product name
+        val productName = "ps"
+        val serviceName = span.clientSideEndpoint.get.serviceName
+
+        val metaFields = Map(
+          "trace_id" -> span.traceId,
+          "span_name" -> span.name,
+          "host" -> span.clientSideEndpoint.get.getHostAddress,
+          "service_name" -> serviceName,
+          "start_time" -> span.firstTimestamp.get,
+          "duration" -> span.duration.get
+        )
+        // filter out xxx.log in binaryAnnotation
+        // decode binaryAnnotation value
+        //val binaryAnnotationFields = span.binaryAnnotations.filter(!_.key.endsWith("log")).
+        //  map( b => b.key -> decoder.decode(b.value))
+
+        Future {
+          client.execute {
+            // index metaFields & binaryAnnotationFields
+            index into duration_index / productName fields metaFields
+          }
+          // return Unit
+          // ToDo: find a way , return Future[Unit]
+          ()
+        } onSuccess { (_) =>
+          //println("send to es success")
+          //println(span.traceId)
+        } onFailure { (_) =>
+          println("error")
+        }
+      } else {
+        Future.Unit
+      }
+
+    } else {
+      Future.Unit
     }
+
   }
   def setTimeToLive(traceId: Long, ttl: Duration): Future[Unit] = {
     Future.Unit
@@ -77,5 +93,13 @@ case class ElasticsearchStorage(
 
   def tracesExist(traceIds: Seq[Long]): Future[Set[Long]] = {
     Future(Set.empty[Long])
+  }
+
+  def getFanoutSpansByIds(ids: Seq[String]): Future[Seq[Seq[Span]]] = {
+    Future(Seq(Seq.empty[Span]))
+  }
+
+  def getFanoutSpansById(id: String): Future[Seq[Span]] = {
+    Future(Seq.empty[Span])
   }
 }
